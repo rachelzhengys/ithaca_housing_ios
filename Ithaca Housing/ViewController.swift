44f05613c4a5c115c727968bbbde4b193312036a
Yisu Zheng <yz689@cornell.edu>
2019-04-22 23:15:53 -0400

    Initial Commit

---
+++ Ithaca Housing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ithaca Housing
+//
+//  Created by RachelZheng on 22/4/2019.
+//  Copyright © 2019 YISU ZHENG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
