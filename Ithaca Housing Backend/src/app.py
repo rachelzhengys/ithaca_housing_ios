e08f779aca5333c4cdb87043eaab0808f78e7599
Zheng Wang <zw494@cornell.edu>
2019-05-05 01:17:19 -0400

    json and file changes

--- Ithaca Housing Backend/src/app.py
+++ Ithaca Housing Backend/src/app.py
@@ -1,6 +1,7 @@
 import json
-from flask import Flask, request
+from flask import Flask, request, url_for
 from db import db, House
+from sqlalchemy import desc
 
 app = Flask(__name__)
 
@@ -15,9 +16,20 @@ with app.app_context():
     db.create_all()
 
 
-@app.route("/api/houses/")
+@app.route("/api/houses")
 def get_houses():
-    houses = House.query.all()
+    sort_options = request.args.get("sort_options", default="0")
+    if sort_options == "0":
+        houses = House.query.all()
+    # elif sort_options == 1:
+    #     houses = House.query.order_by(House.postdate).all()
+    elif sort_options == "2":
+        houses = House.query.order_by(House.price).all()
+    elif sort_options == "3":
+        houses = House.query.order_by(desc(House.price)).all()
+    else:
+        res = {"success": False, "data": "invalid_sort_options"}
+        return json.dumps(res), 404
     res = {"success": True, "data": [house.serialize() for house in houses]}
     return json.dumps(res), 200
 
@@ -27,24 +39,29 @@ def get_house_by_id(house_id):
     house = House.query.filter_by(id=house_id).first()
     if house is None:
         return json.dumps({"success": False, "error": "House not found!"}), 404
-
     res = {"success": True, "data": house.serialize()}
     return json.dumps(res)
 
 
 @app.route("/api/house/", methods=["POST"])
 def post_house():
-    post_body = json.loads(request.data)
-    house = House(
-        image=post_body.get("image"),
-        location=post_body.get("location"),
-        housing_type=post_body.get("type"),
-        contact=post_body.get("contact"),
-        price=post_body.get("price"),
-    )
-    db.session.add(house)
+    post_bodies = json.loads(request.data)
+    house = [
+        House(
+            imageurl=post_body.get("imageurl"),
+            location=post_body.get("location"),
+            housing_type=post_body.get("type"),
+            # contact=post_body.get("contact"),
+            price=post_body.get("price"),
+            postdate=post_body.get("postdate"),
+            houseurl=post_body.get("url"),
+            description=post_body.get("description"),
+        )
+        for post_body in post_bodies
+    ]
+    db.session.bulk_save_objects(house)
     db.session.commit()
-    return json.dumps({"success": True, "data": house.serialize()}), 201
+    return json.dumps({"success": True, "data": post_bodies}), 201
 
 
 @app.route("/api/house/<int:house_id>/", methods=["DELETE"])
--- Ithaca Housing Backend/src/db.py
+++ Ithaca Housing Backend/src/db.py
@@ -1,39 +1,88 @@
 import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Table, Column, Integer, ForeignKey, DateTime
-from sqlalchemy.orm import relationship
+from sqlalchemy import Table, Column, Integer, ForeignKey, DateTime, desc, MetaData
+from sqlalchemy.orm import relationship, mapper
 
 db = SQLAlchemy()
 
+mymetadata = MetaData()
+
+# columns = (
+#     db.Column("houseurl", db.String, primary_key=True),
+#     db.Column("imageurl", db.String, nullable=True),
+#     db.Column("location", db.String, nullable=False),
+#     # subject to change: may be api of google maps
+#     db.Column("type", db.String, nullable=False),  # subject to change
+#     db.Column("price", db.Integer, nullable=False),  # tbd
+#     db.Column("description", db.String, nullable=False),
+#     # contact = db.Column(db.String, nullable=False)
+#     # this should be changed to user id
+#     db.Column("postdate", db.String, nullable=False),
+# )
+
+# jsontable = Table("house_in_json", mymetadata, *columns)
+
+
+# class House(object):
+#     def __init__(self, json_data):
+#         json_data = open("house.json")
+#         data = json.load(json_data)
+#         for key, value in data.iteritems():
+#             setattr(self, key, value)
+
+#     def serialize(self):
+#         return {
+#             # "id": self.id,
+#             "location": self.location,
+#             "type": self.housing_type,
+#             "price": self.price,
+#             # "contact": self.contact,
+#             "postdate": self.postdate,
+#             "imageurl": self.imageurl,
+#             "description": self.description,
+#             "houseurl": self.houseurl,
+#         }
+
+
+# mapper(House, jsontable)
+
 
 class House(db.Model):
-    __tablename__ = "posts"
+    __tablename__ = "houses"
     id = db.Column(db.Integer, primary_key=True)
-    image = db.Column(db.String, nullable=True)
+    houseurl = db.Column(db.String, nullable=False)
+    imageurl = db.Column(db.String, nullable=True)
     location = db.Column(db.String, nullable=False)
     # subject to change: may be api of google maps
     housing_type = db.Column(db.String, nullable=False)  # subject to change
     price = db.Column(db.Integer, nullable=False)  # tbd
-    contact = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    # contact = db.Column(db.String, nullable=False)
     # this should be changed to user id
-    postdate = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    postdate = db.Column(db.String, nullable=False)
 
     def __init__(self, **kwargs):
-        self.image = kwargs.get("image", "")
+        self.imageurl = kwargs.get("imageurl", "")
+        self.houseurl = kwargs.get("houseurl", "")
         self.location = kwargs.get("location", "")
+        self.description = kwargs.get("description", "")
         self.housing_type = kwargs.get("housing_type", "")
         self.price = kwargs.get("price", 0)
-        self.contact = kwargs.get("contact", "")
+        # self.contact = kwargs.get("contact", "")
+        self.postdate = kwargs.get("postdate", "")
+
         # TODO look up image library: https://sqlalchemy-imageattach.readthedocs.io/en/1.0.0/
 
     def serialize(self):
         return {
-            "id": self.id,
+            # "id": self.id,
             "location": self.location,
             "type": self.housing_type,
             "price": self.price,
-            "contact": self.contact,
-            "postdate": self.postdate.__str__(),
-            "image": self.image,
+            # "contact": self.contact,
+            "postdate": self.postdate,
+            "imageurl": self.imageurl,
+            "description": self.description,
+            "houseurl": self.houseurl,
         }
